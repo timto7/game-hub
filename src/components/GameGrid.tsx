a825fffef0cc8aeeabb9989f27d9a28464aeeee0
Tim Knight <timmy.knight@gmail.com>
2023-09-12 20:42:36 +0100

    Display platfrom icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,8 +11,8 @@ const GameGrid = () => {
       {error && <Text color="red">{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        spacing={10}
         padding={10}
+        spacing={10}
       >
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import { HStack, Icon } from '@chakra-ui/react';
+
+import {
+  FaAndroid,
+  FaApple,
+  FaLinux,
+  FaPlaystation,
+  FaWindows,
+  FaXbox,
+} from 'react-icons/fa';
+import { MdPhoneIphone } from 'react-icons/md';
+import { SiNintendo } from 'react-icons/si';
+import { BsGlobe } from 'react-icons/bs';
+
+import { Platform } from '../hooks/useGames';
+import { IconType } from 'react-icons';
+
+interface Props {
+  platforms: Platform[];
+}
+
+const iconMap: { [key: string]: IconType } = {
+  android: FaAndroid,
+  pc: FaWindows,
+  playstation: FaPlaystation,
+  xbox: FaXbox,
+  nintendo: SiNintendo,
+  mac: FaApple,
+  linux: FaLinux,
+  ios: MdPhoneIphone,
+  web: BsGlobe,
+};
+
+const PlatformIconList = ({ platforms }: Props) => {
+  return (
+    <HStack marginY="10px">
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
