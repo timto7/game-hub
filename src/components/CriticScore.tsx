f05cd7cd630b9dd3c0c445c83f215b88d5dbeb94
Tim Knight <timmy.knight@gmail.com>
2023-09-12 20:51:47 +0100

    Display critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,23 @@
+import { useMemo } from 'react';
+
+import { Badge } from '@chakra-ui/react';
+
+interface Props {
+  score: number;
+}
+
+const CriticScore = ({ score }: Props) => {
+  const color = useMemo(() => {
+    if (score > 75) return 'green';
+    if (score > 60) return 'green';
+    return '';
+  }, []);
+
+  return (
+    <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
+      {score}
+    </Badge>
+  );
+};
+
+export default CriticScore;
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -15,6 +15,7 @@ export interface Game {
   name: string;
   background_image: string;
   parent_platforms: { platform: Platform }[];
+  metacritic: number;
 }
 
 export interface FetchGamesResponse {
